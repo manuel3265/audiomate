a623471b6dbe3a2d96a270fa4c9157b06ad4e6bb
buec <m.buechi@outlook.com>
2019-11-15 23:10:07 +0100

    test

--- audiomate/corpus/io/nvidia_jasper.py
+++ audiomate/corpus/io/nvidia_jasper.py
@@ -76,75 +76,92 @@ class NvidiaJasperWriter(base.CorpusWriter):
     def process_utterances(self, corpus, base_path):
         os.makedirs(os.path.join(base_path, 'audio'), exist_ok=True)
 
-        utts = []
-
-        for utterance in tqdm(corpus.utterances.values()):
-            utts.append(self.process_utterance(utterance, base_path))
-
+        utts = self.process_utterance(corpus.utterances.values(), base_path)
         return utts
 
     def process_utterances_parallel(self, corpus, base_path):
+        in_utts = list(corpus.utterances.values())
+        num_utts = len(in_utts)
+        utts_per_worker = int(num_utts / self.num_workers)
+
+        splits = []
+
+        for i in range(self.num_workers - 1):
+            start = i * utts_per_worker
+            end = (i + 1) * utts_per_worker
+            splits.append(in_utts[start:end])
+
+        splits.append(in_utts[end:])
+
         with multiprocessing.Pool(self.num_workers) as p:
             func = functools.partial(
                 self.process_utterance,
                 base_path=base_path
             )
 
-            in_utts = list(corpus.utterances.values())
-            utts = list(tqdm(p.imap(func, in_utts), total=len(in_utts)))
+            split_res = list(p.imap(func, splits))
 
-        utts = [u for u in utts if u[1] is not None]
+        utts = []
+
+        for r in split_res:
+            utts.extend(r)
+
         return utts
 
-    def process_utterance(self, utt, base_path):
-        export_utt = False
+    def process_utterance(self, utts, base_path):
+        res = []
 
-        if self.export_all_audio:
-            export_utt = True
+        for utt in tqdm(utts):
+            export_utt = False
 
-        elif utt.start != 0 or utt.end != float('inf'):
-            export_utt = True
+            if self.export_all_audio:
+                export_utt = True
 
-        elif type(utt.track) != tracks.FileTrack:
-            export_utt = True
+            elif utt.start != 0 or utt.end != float('inf'):
+                export_utt = True
 
-        elif not utt.track.path.endswith('wav'):
-            export_utt = True
+            elif type(utt.track) != tracks.FileTrack:
+                export_utt = True
 
-        elif utt.sampling_rate != 16000:
-            export_utt = True
+            elif not utt.track.path.endswith('wav'):
+                export_utt = True
 
-        if export_utt:
-            file_path = os.path.join(
-                base_path,
-                'audio',
-                '{}.wav'.format(utt.idx)
-            )
+            elif utt.sampling_rate != 16000:
+                export_utt = True
 
-            samples = utt.read_samples(16000)
-            audio.write_wav(file_path, samples, 16000)
+            if export_utt:
+                file_path = os.path.join(
+                    base_path,
+                    'audio',
+                    '{}.wav'.format(utt.idx)
+                )
 
-            num_samples = len(samples)
-            duration = num_samples / 16000
-        else:
-            file_path = utt.track.path
-            num_samples = utt.num_samples()
-            duration = utt.duration
+                samples = utt.read_samples(16000)
+                audio.write_wav(file_path, samples, 16000)
 
-        rel_path = os.path.relpath(file_path, base_path)
+                num_samples = len(samples)
+                duration = num_samples / 16000
+            else:
+                file_path = utt.track.path
+                num_samples = utt.num_samples()
+                duration = utt.duration
 
-        out_dict = {
-            'transcript': utt.label_lists[self.transcription_label_list_idx].join(),
-            'files': [{
-                'fname': rel_path,
-                'channels': 1,
-                'sample_rate': 16000,
-                'duration': duration,
-                'num_samples': num_samples,
-                'speed': 1
-            }],
-            'original_duration': duration,
-            'original_num_samples': num_samples
-        }
+            rel_path = os.path.relpath(file_path, base_path)
 
-        return (utt.idx, out_dict)
+            out_dict = {
+                'transcript': utt.label_lists[self.transcription_label_list_idx].join(),
+                'files': [{
+                    'fname': rel_path,
+                    'channels': 1,
+                    'sample_rate': 16000,
+                    'duration': duration,
+                    'num_samples': num_samples,
+                    'speed': 1
+                }],
+                'original_duration': duration,
+                'original_num_samples': num_samples
+            }
+
+            res.append((utt.idx, out_dict))
+
+        return res
